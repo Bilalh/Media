d002a13e2022b441593bd977a2368356fc0c8bef
Bilal Hussain <bhterra@gmail.com>
2010-05-18 23:50:51 +0000

    mplayer
    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@4 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- media.c
+++ media.c
@@ -12,50 +12,57 @@
 #include "Media.h"
 
 #define DIRENT(value) (*(struct dirent **) value)
-#define PATTEN ".*\\.mkv"
+#define VIDEO ".*\\.mkv|mp4|mov|ogm|avi|divx|rm|rmvb|wmv|flv|part$"
+#define AUDIO  ".*\\.mp3|m4a|flac|ogg|m4b|aiff|ac3|aac|wma|wav"				
+                              
+void 
+	mplayer(char **filenames, int length, char *prefix_args, char *postfix_args, char *filepath);
+
 void media(char *path) {
 	struct dirent **files;
 
 	// gets dir listing ignoring case and hidden files
 	int file_num = scandir_b( path, &files,
 		^ (struct dirent * s) { 
-			// return (s->d_name[0] != '.'); 
-			return match(s->d_name ,PATTEN);
+			return match(s->d_name ,VIDEO);
 		},
 		^ (const void * a, const void * b) {
 			return strcasecmp( DIRENT(a)->d_name, DIRENT(b)->d_name);
 		}
 	);
 	
-	int path_len = strlen(path)+3 ;// 3 extra for,cd ; 
-	int cur_len  = 50 * file_num + path_len;
-	
-	// builds the argument fro mplayer
-	char m_args[cur_len];              // args for mplayer
-	int index = path_len;              // index in string
-	strcpy(m_args,"cd ");              // cd command
-	strcpy(&m_args[3],path);	       
+
+	char *sa[file_num];
+	for(int i =0 ; i<file_num;i++) sa[i] = files[i]->d_name; 
+	mplayer(sa,4,"","",path);
+}
+
+//FIXME add length  
+//TODO use struct
+void 
+mplayer(char **filenames, int length, char *prefix_args, char *postfix_args, char *filepath){
+   	int index = strlen(filepath)+3;
+	char m_args[strlen(filenames[0])*10+strlen(prefix_args)+strlen(postfix_args)+index+8];  
+	strcpy(m_args,"cd ");              
+	strcpy(&m_args[3],filepath);	       
 	m_args[index++] = ';';             
-	m_args[index++] = ' ';             
+	m_args[index++] = ' ';
 	strcpy(&m_args[index],"mplayer "); // mplayer 
-	index+=8;                                   
-	
-	for(int i = 0; i < file_num; i++) {
-		printf("%s	\n", files[i]->d_name);
-		// copies each filename into m_args
+	index+=8;
+
+	// copies each filename into m_args
+	for(int i = 0; i < length; i++) {
+		printf("%s	\n", filenames[i]); 		
 		m_args[index++]='"';
-		strcpy(&m_args[index], files[i]->d_name);
-		index += strlen(files[i]->d_name);
+		strcpy(&m_args[index], filenames[i]);
+		index += strlen(filenames[i]);
 		m_args[index++]='"';
 		m_args[index++]=' ';
-	}                        
-	
+	}
 	m_args[index] = '\0';
 	printf("%s\n", m_args);
-	system(m_args);       
-	
-}
-                                            
+	system(m_args);
+}                                            
 
 /// \brief 1 on match, 0 on any error s
 int match(const char *string, char *pattern) {
@@ -67,8 +74,7 @@ int match(const char *string, char *pattern) {
 	}
 	status = regexec(&re, string, (size_t) 0, NULL, 0);
 	regfree(&re);
-	if (status != 0) {
-		return(0);      
-	}
-	return(1);
+	
+	return !(status !=0);
+	
 }
