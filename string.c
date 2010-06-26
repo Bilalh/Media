8b60d3fb3ea4b7001cc03d096898d747d926126e
Bilal Hussain <bhterra@gmail.com>
2010-06-26 23:32:45 +0000

    fixed realloc bug
    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@37 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- string.c
+++ string.c
@@ -7,9 +7,9 @@ void string_push(String *s, char *str) {
 	int str_len = strlen(str);
 
 	printf("a in %i len %i \n", s->index, s->length);
-	if ( (s->length - s->index) < str_len ) {
+	if ( (s->length - s->index) < str_len +1 ) { // 1 for \0
 		s->length += str_len * 2 + 2; // 2 for /0 and space
-		realloc(s->str, s->length);
+		s->str = realloc(s->str, s->length);
 	}
 	printf("b in %i len %i \n", s->index, s->length);
 
