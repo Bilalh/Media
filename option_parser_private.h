31bcbeeda7842bf836794a3aa8eec4db4f70d5d4
Bilal Hussain <bhterra@gmail.com>
2010-07-02 21:53:33 +0000

    better doc for opts
    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@43 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- option_parser_private.h
+++ option_parser_private.h
@@ -11,36 +11,57 @@ const Element H_playlist[] ={
 };
 const Element H_player[] ={
 	
-	{{"mplayer", no_argument, 0, 'm'}, "", "plays file(s) using mplayer",
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "mplayer", .val = 'm', .has_arg = no_argument, .flag = 0}, 
+		.help  = "plays the files using mplayer",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			ma->player = P_MPLAYER;
 		}
 	},
-	
-	{{"niceplayer", no_argument, 0, 'N'}, "", "plays file(s) using niceplayer",
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "niceplayer", .val = 'N', .has_arg = no_argument, .flag = 0}, 
+		.help  = "plays the files using niceplayer",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			ma->player = P_NICEPLAYER;
 		}
 	},
-	
-	{{"vlc", no_argument, 0, 'V'}, "", "plays file(s) using vlc",
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "vlc", .val = 'V', .has_arg = no_argument, .flag = 0}, 
+		.help  = "plays the files using vlc",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			ma->player = P_VLC;
 		}
-	}
+	},
 	
 };
 
 const Element H_mplayer[] = { 
 	
-	{{"fs", no_argument, 0, 'f'}, "", "plays file(s) in fullscreen",
-		^ (MediaArgs *ma ) {
+	{  
+		.opt   = {.name =  "fs", .val = 'f', .has_arg = no_argument, .flag = 0, }, 
+		.help  = "plays file(s) in fullscreen",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			string_push(&ma->prefix_args,"-fs");
 		}
 	},
-	
-	{{"top", no_argument, 0, 't'}, "", "Add profile t, afloat and 360p",
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "top", .val = 't', .has_arg = no_argument, .flag = 0}, 
+		.help  = "Adds profile t  - afloat, all spaces and 360p",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
+			string_push(&ma->prefix_args, "-profile t");
+			ma->afloat = true;
+		}
+	},
+	{  
+		.opt   = {.name =  "mtop", .val = 'T', .has_arg = no_argument, .flag = 0}, 
+		.help  = "Adds profile T  - ontop and 360p",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			string_push(&ma->prefix_args, "-profile t");
 			ma->afloat = true;
 		}
@@ -54,14 +75,19 @@ const Element H_output[] ={
 
 const Element H_other[] ={
 	
-	{{"help", no_argument, 0, 'h'}, "", "Shows the help",
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "help", .val = 'h', .has_arg = no_argument, .flag = 0}, 
+		.help  = "",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			print_help();
 		}
 	},
-	
-	{{"print_opt", no_argument, 0, 'Z'}, "", "Shows the opt struct"	,
-		^ (MediaArgs *ma){
+	{  
+		.opt   = {.name =  "print_opt", .val = 'Z', .has_arg = no_argument, .flag = 0}, 
+		.help  = "Shows the opt struct",
+		.arg   = "",
+		.block = ^(MediaArgs *ma ) {
 			print_media_args(ma);
 		}
 	}
