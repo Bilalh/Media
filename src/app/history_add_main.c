0257aca6a02258171cfc2de0a10df06fd07a290b
Bilal Hussain <bhterra@gmail.com>
2010-09-19 22:26:11 +0000

    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@179 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- src/app/history_add_main.c
+++ src/app/history_add_main.c
@@ -9,11 +9,13 @@
 #include <include/string_buffer.h>
 #include <include/debug.h>
 
-//"hista Series lower_num [time] [upper_num] [sep] "
+#define USAGE "hista Series lower_num [time] [upper_num] [sep|t] [score|t] [t]"
+
+//usage hista Series lower_num [time] [upper_num] [sep] 
 int main (int argc, char *argv[]) {
 
 	if( argc < 3 ) {
-		printf("%s\n", "hista Series lower_num [time] [upper_num] [sep] ");
+		printf("%s\n", USAGE);
 		exit(0);
 	}
 
@@ -23,7 +25,7 @@ int main (int argc, char *argv[]) {
 	int lower = (int) strtol(argv[2], NULL, 10);
 	if( lower == 0 ) {
 		printf("%s %s\n", "Invalid number", argv[2] );
-		printf("%s\n", "hista Series lower_num [time] [upper_num] [sep] ");
+		printf("%s\n", USAGE);
 		exit(1);
 	}
 
@@ -56,12 +58,12 @@ int main (int argc, char *argv[]) {
 		upper = (int) strtol(argv[4], NULL, 10);
 		if( upper == 0 ) {
 			printf("%s %s\n", "Invalid number", argv[4] );
-			printf("%s\n", "hista Series lower_num [time] [upper_num] [sep] ");
+			printf("%s\n", USAGE);
 			exit(2);
 		}
 		if (upper < lower){
 			printf("Upper has to be >= then lower\n");
-			printf("%s\n", "hista Series lower_num [time] [upper_num] [sep] ");
+			printf("%s\n", USAGE);
 			exit(3);
 		}
 		
@@ -96,12 +98,34 @@ int main (int argc, char *argv[]) {
 		sep = (int) strtol(argv[5], NULL, 10);
 		if( sep == 0 ) {
 			printf("%s %s\n", "Invalid number", argv[5] );
-			printf("%s\n", "hista Series lower_num [time] [upper_num] [sep] ");
+			printf("%s\n", USAGE);
 			exit(1);
 		}
 	}
 	
+	int score = 0;
 	if( argc >= 7 ){
+		if( *argv[6] == 't' ){
+			char buff[28];
+			strftime(buff, 28, "%Y-%m-%d %H:%M %a %d %b", tm);
+			printf("  Title: %s\n", series );
+			printf("  Num_L: %d\n", lower );
+			printf("  Num_U: %d\n", upper );
+			printf("   Time: %s\n", buff );
+			printf("    Sep: %d\n", sep );
+			exit(0);	
+		}else{
+			score = (int) strtol(argv[6], NULL, 10);
+			if( score == 0 ) {
+				printf("%s %s\n", "Invalid score", argv[6] );
+				printf("%s\n", USAGE);
+				exit(1);
+			}
+		}
+		
+	}
+	
+	if( argc >= 8 ){
 		char buff[28];
 		strftime(buff, 28, "%Y-%m-%d %H:%M %a %d %b", tm);
 		printf("  Title: %s\n", series );
@@ -109,6 +133,7 @@ int main (int argc, char *argv[]) {
 		printf("  Num_U: %d\n", upper );
 		printf("   Time: %s\n", buff );
 		printf("    Sep: %d\n", sep );
+		printf("  Score: %d\n", score );
 		exit(0);
 	}
 	
@@ -117,16 +142,25 @@ int main (int argc, char *argv[]) {
 	for(int i = lower; i <=upper; ++i){
 		char time_buff[20];
 		MAKE_TIME_STR(time_buff,tm);
-		string_sprintf(&s, 87 + strlen(series),
-			"Insert Into History(Series,Date,Number) "
-			"Values('%s', '%s', %d); ",
+		string_sprintf(&s, 87 + strlen(series) + 1,
+			"INSERT INTO History(Series,Date,Number) "
+			"VALUES('%s', '%s', %d); ",
 			 series, time_buff, i
 		);
 		tm->tm_min += sep;
 		timegm(tm);
 	}
-	sql_exec(s.str, NULL);
 	
+	if( score > 0 ){
+		string_sprintf(&s, 18+ 17 + 21  + strlen(series) +1,
+			" UPDATE SeriesInfo"
+			" SET Score   = '%d'"
+			" WHERE Title = '%s'; ",
+			score, series
+		);
+	}
+	
+	sql_exec(s.str, NULL);
 	free(tm);
 	return 0;
 }
