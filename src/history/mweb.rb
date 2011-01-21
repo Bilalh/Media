034d2486b9a15e2108ec6cc43e58043903f504a0
Bilal Hussain <bhterra@gmail.com>
2011-01-21 00:32:41 +0000

    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@279 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- src/history/mweb.rb
+++ src/history/mweb.rb
@@ -9,5 +9,5 @@ ep = Integer(url[/ep=(\d+)/,1])
 new_url = url.gsub(/ep=\d+/,"ep=#{ep+1}")
 `osascript -e 'tell application "Safari"' -e 'set URL in document 1 to "#{new_url}"' -e 'end tell'`
 
-puts "$HOME/bin/hista '#{series}' #{ep+1}, '1 min ago' #{ep} #{ARGV.join " "}"
-puts `$HOME/bin/hista '#{series}' #{ep+1}, '1 min ago' #{ep} #{ARGV.join " "}`
+puts "$HOME/bin/hista '#{series}' #{ep+1}, '1 min ago' #{ep+1} #{ARGV.join " "}"
+puts `$HOME/bin/hista '#{series}' #{ep+1}, '1 min ago' #{ep+1} #{ARGV.join " "}`
