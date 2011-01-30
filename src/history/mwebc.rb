e43a2081fb0b15a7c3d7fab9ed8a7f5b0c15e2da
Bilal Hussain <bhterra@gmail.com>
2011-01-30 01:56:22 +0000

    git-svn-id: file://localhost/Users/bilalh/Programming/svn/c/media@284 daa31a9f-09b6-45a9-b2f2-c952b89d5742

--- src/history/mwebc.rb
+++ src/history/mwebc.rb
@@ -6,5 +6,5 @@ url = `osascript -e 'tell application "Safari"'  -e 'set my_URL to the URL in do
 series  = url[/a=([-\w~_]+)/,1].tr '_', ' '
 ep = Integer(url[/ep=(\d+)/,1])
 
-puts "$HOME/bin/hista '#{series}' #{ep} '1 min ago' #{ep} #{ARGV.join " "}"
-puts `$HOME/bin/hista '#{series}' #{ep} '1 min ago' #{ep} #{ARGV.join " "}`
+puts "$HOME/bin/hista '#{series}' #{ep} '1 min ago' #{ep} 27 #{ARGV.join " "}"
+puts `$HOME/bin/hista '#{series}' #{ep} '1 min ago' #{ep} 27 #{ARGV.join " "}`
