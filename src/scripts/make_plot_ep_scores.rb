d8a77a09b6a42293146282ca4cf5eb01702f7c6c
Bilal Syed Hussain <bhterra@gmail.com>
2011-12-18 05:01:45 +0000

    plot

--- src/scripts/make_plot_ep_scores.rb
+++ src/scripts/make_plot_ep_scores.rb
@@ -1,7 +1,10 @@
 #!/usr/bin/env ruby -wKU
 
 DB = File.expand_path "/Users/bilalh/Library/Application Support/Media/Media.db"
+SQL = <<-SQL
+Select si.Total, si.Score From SeriesData si 
+Where si.Score IS NOT NULL And si.Score > 0 And si.Total IS NOT NULL 
+Order by si.Total
+SQL
 
-puts `sqlite3 "#{DB}" 'select total,score from  FinishedSeries fs order by fs.Total;' | sed 's/|/ /'`
-
-
+puts `sqlite3 "#{DB}" '#{SQL};' | sed 's/|/ /'`
