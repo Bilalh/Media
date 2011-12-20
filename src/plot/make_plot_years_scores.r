35f866032a6c853a1893d6cc007b86a9580aa648
Bilal Syed Hussain <bhterra@gmail.com>
2011-12-20 19:00:52 +0000

    finished sql and plots

---
+++ src/plot/make_plot_years_scores.r
@@ -0,0 +1,30 @@
+#!/usr/bin/Rscript
+name="plot_years_scores"
+
+data<-read.table(paste("data_", name, sep=""), header=T, sep="\t")
+# names(data) <-c("scores", "counts")
+# attach(data)
+# 
+pdf(file=paste(name, ".pdf", sep="") )
+
+colours=terrain.colors(10)
+barplot(t(data),
+	xlab="Year",  
+	ylab="Count",
+	main="Distribution of Scores by Year",
+	col=colours,
+	names.arg=c(1979,1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011),
+)
+legend("topleft", names(data), cex=0.6, fill=colours)
+
+
+# pdf(file=paste(name, "_chart.pdf", sep=""))
+# 
+# colours = rainbow(length(scores))
+# 
+# pie(counts,
+# 	labels=counts,
+# 	main="Distribution of Years",
+# 	col=colours
+# )
+# 
